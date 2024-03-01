fa64824fbde59902454229872bd18efa2cea67e9
theolao <156953040+theolao@users.noreply.github.com>
2024-02-29 23:52:01 -0500

    Add files via upload

---
+++ Couple.java
@@ -0,0 +1,15 @@
+public class Couple {
+    private Person bride;
+    private Person groom;
+    public Couple(Person bride, Person groom) {
+        this.bride = bride;
+        this.groom = groom;
+    }
+    public Person getBride() {
+        return bride;
+    }
+    public Person getGroom() {
+        return groom;
+    }
+}
+
