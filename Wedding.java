25f272ba39ee9f686b6c5ebdb80bbdec1381d425
theolao <156953040+theolao@users.noreply.github.com>
2024-02-29 23:53:47 -0500

    Add files via upload

---
+++ Wedding.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+public class Wedding {
+    private Couple couple;
+    private LocalDate weddingDate;
+    private String location;
+    public Wedding(Couple couple, LocalDate weddingDate, String location) {
+        this.couple = couple;
+        this.weddingDate = weddingDate;
+        this.location = location;
+    }
+    public Couple getCouple() {
+        return couple;
+    }
+    public LocalDate getWeddingDate() {
+        return weddingDate;
+    }
+    public String getLocation() {
+        return location;
+    }
+}
+
